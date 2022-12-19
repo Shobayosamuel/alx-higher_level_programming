e44545727467a77419252ea884ccbac65a431ed0
Shobayosamuel <shobayosamuel62@gmail.com>
2022-12-19 23:28:06 +0100

    safe print integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
+
+
 def safe_print_integer(value):
     try:
-        li = []
-        li1 = ["a", "b", "c", "d", "e", "f", 'g', "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-        li2 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-        for i in range(len(value)):
-            li.append(value[i])
-        if (li[0] in li1) or (li[0] in li2):
-            return False
-
-
-    except:
         print("{:d}".format(value))
-        return True
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
