b8c7d645874ff222571fc03a1c75f5f8d5317bc3
Shobayosamuel <shobayosamuel62@gmail.com>
2023-01-02 13:23:30 +0100

    Removed white spaces

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,7 +4,6 @@
 
 class Rectangle:
     """Represent a triangle"""
-    
     def __init__(self, width=0, height=0):
         """Represent the triangle
 
@@ -14,42 +13,33 @@ class Rectangle:
         """
         self.__width = width
         self.__height = height
-    
+
     @property
     def width(self):
         """Get the width of the triangle"""
-        
         return (self.__width)
-    
+
     @width.setter
     def width(self, value):
         """set the width of the triangle according to the conditions"""
-        
         if type(value) != int:
             raise TypeError("width must be an integer")
-        
+
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @property
     def height(self):
         """Get the height of the triangle"""
-        
         return (self.__height)
-    
+
     @height.setter
     def height(self, value):
         """Set the height of the triangle"""
-        
         if type(value) != int:
             raise TypeError("height must be an integer")
-        
+
         if value < 0:
             raise ValueError("height must be >= 0")
-        
         self.__height = value
-        
-        
-my_rectangle = Rectangle(2, 4)
-print(my_rectangle.__dict__)
