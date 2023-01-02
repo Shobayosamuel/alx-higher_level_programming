2f72d4234b982f650db5e843978903ac8bbcc117
Shobayosamuel <shobayosamuel62@gmail.com>
2023-01-02 15:35:51 +0100

    Added a class method square()

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """Defines a Rectangle class."""
 
 
@@ -116,7 +117,3 @@ class Rectangle:
             Rectangle: with width and height = size
         """
         return (cls(size, size))
-
-my_square = Rectangle.square(5)
-print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
-print(my_square)
