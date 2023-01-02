6eeb52b4b840fa0ab9ccaed903448be2b85c4fca
Shobayosamuel <shobayosamuel62@gmail.com>
2023-01-02 22:09:21 +0100

    say my name

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def matrix_divided(matrix, div):
     length = 0
     width = 0
@@ -10,13 +11,12 @@ def matrix_divided(matrix, div):
                         "integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
-    
+
     if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
-    
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
     new = [list(map(lambda x: round(x / div, 2), row)) for row in matrix]
     return (new)
-
