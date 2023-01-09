6e5d5a4c0745dac90a66f4a75430e00d096d4b5c
Shobayosamuel <shobayosamuel62@gmail.com>
2023-01-09 18:56:50 +0100

    My List

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+"""Define a class MyList"""
+
+
+class MyList(list):
+    """MyList inherits from list"""
+    def __init__(self):
+        """initialize the class and make a super class"""
+        super().__init__()
+    def print_sorted(self):
+        """Return a sorted List"""
+        print(sorted(self))
