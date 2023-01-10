a024dff8339fa3c59dc19ed3e4eb99614693a1d4
Shobayosamuel <shobayosamuel62@gmail.com>
2023-01-11 00:04:30 +0100

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+"""Define the function to_json_string"""
+
+
+def to_json_string(my_obj):
+    """Return: the json representaion of the string"""
+    return json.dumps(my_obj)
