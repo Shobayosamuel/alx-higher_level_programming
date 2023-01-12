c8771e520e5a354d3af7b5e31aac43cd01162459
Shobayosamuel <shobayosamuel62@gmail.com>
2023-01-12 10:59:38 +0100

    tests/

--- 3-print_number.py
+++ 3-print_number.py
@@ -1,32 +1,6 @@
+import numpy as np
+def lazy_matrix_mul(m_a, m_b):
+    return (np.matmul(m_a, m_b))
 
-class Rectangle:
-    def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
-    
-    @property
-    def width(self):
-        return (self.__width)
-    
-    @width.setter
-    def width(self, value):
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-    
-    @property
-    def height(self):
-        return (self.__height)
-    
-    @height.setter
-    def height(self, value):
-        if type(value) != int:
-            raise TypeError("height must be an integer")
-        
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        
-        self.__height = value
+print(lazy_matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
+print(lazy_matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
