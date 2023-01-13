6408e725dbaa380e7a2f11be0c13a38b625e63f2
Shobayosamuel <shobayosamuel62@gmail.com>
2023-01-13 23:21:17 +0100

    Done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -46,6 +46,11 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
+        """Create a new instance
+
+        Returns:
+            class: 
+        """
         if dictionary and dictionary != {}:
             if cls.__name__ == "Rectangle":
                 new_instance = cls(1, 1)
@@ -53,3 +58,56 @@ class Base:
                 new_instance = cls(1)
         new_instance.update(**dictionary)
         return (new_instance)
+
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of classes instantiated
+
+        Returns:
+            list: List of instantiated classes
+        """
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as fil:
+                list_dicts = Base.from_json_string(fil.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
+
+if __name__ == "__main__":
+    from rectangle import Rectangle
+    from square import Square
+    r1 = Rectangle(10, 7, 2, 8)
+    r2 = Rectangle(2, 4)
+    list_rectangles_input = [r1, r2]
+
+    Rectangle.save_to_file(list_rectangles_input)
+
+    list_rectangles_output = Rectangle.load_from_file()
+
+    for rect in list_rectangles_input:
+        print("[{}] {}".format(id(rect), rect))
+
+    print("---")
+
+    for rect in list_rectangles_output:
+        print("[{}] {}".format(id(rect), rect))
+
+    print("---")
+    print("---")
+
+    s1 = Square(5)
+    s2 = Square(7, 9, 1)
+    list_squares_input = [s1, s2]
+
+    Square.save_to_file(list_squares_input)
+
+    list_squares_output = Square.load_from_file()
+
+    for square in list_squares_input:
+        print("[{}] {}".format(id(square), square))
+
+    print("---")
+
+    for square in list_squares_output:
+        print("[{}] {}".format(id(square), square))
