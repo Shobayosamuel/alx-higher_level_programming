4c997ccdae1e2bcd203bc617928d6b0c930bedba
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-03-14 00:07:39 +0000

    Second Biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+if (args.length === 0) console.log(0);
+else if (args.length === 1) console.log(0);
+else {
+  const sortedArgs = args.sort((a, b) => b - a);
+  console.log(sortedArgs[1]);
+}
