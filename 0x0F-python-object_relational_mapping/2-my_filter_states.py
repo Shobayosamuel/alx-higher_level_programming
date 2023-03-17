da4ac02977b6d6723ccb0806c186a9010b6d3bea
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-03-17 11:57:48 +0000

    Added MySQLdb import

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,6 +4,7 @@
     states table of hbtn_0e_0_usa where name matches the argument.
     import MySQLdb
 """
+import MySQLdb
 import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], port=3306, host="localhost",
