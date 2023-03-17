9f1ad05d15562f62cd0a2b863ef6e706f5fbee32
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-03-17 13:12:07 +0000

    changed variable db to conn

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,13 +9,13 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-    cursor = db.cursor()
+    cursor = conn.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
                    (sys.argv[4],))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
     cursor.close()
-    db.close()
+    conn.close()
