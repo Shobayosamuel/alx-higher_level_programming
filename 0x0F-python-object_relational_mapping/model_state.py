58b0e52600a8c1ce99cf77164a0a7d46a82a0d9a
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-03-18 15:02:48 +0000

    Added description to my class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
+"""
+    Create a python class State that inherits
+    from Base class and links it to the MySQL
+    table
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
 
+
 class State(Base):
+    """
+        This is a sql query representation
+        in the for of a class
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
