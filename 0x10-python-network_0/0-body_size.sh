26c9f595b791532e19dd1b99d45dfe29852d50a4
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-03-30 23:47:41 +0100

    Ref: Reduced the number of lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# script that takes a url, sends a request to the url and
-# display the size of the body of the response
+# script that takes a url, sends a request to the url and display the size of the body of the response
 curl -s "$1" | wc -c
