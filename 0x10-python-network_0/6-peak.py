54284f2f099df06cd0fbef555fc53e90d32911d6
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-03-31 02:32:57 +0100

    feat: Find peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+"""find the peak of an unsorted array"""
+
+
+def find_peak(list_of_integers):
+    """
+        Return the peak of the array
+    """
+    if not list_of_integers:
+        return None
+    lists = list_of_integers
+    size = len(lists)
+    if size == 1:
+        return lists[0]
+    elif size == 2:
+        return max(lists)
+    mid = int(size / 2)
+    peak = lists[mid]
+    if peak > lists[mid - 1] and peak > lists[mid + 1]:
+        return peak
+    elif peak < lists[mid - 1]:
+        return find_peak(lists[:mid])
+    else:
+        return find_peak(lists[mid + 1:])
