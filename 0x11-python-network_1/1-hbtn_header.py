ce135e93ccde1f88890f0a95c4292381a7368949
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-03-31 23:17:07 +0000

    feat: script that returns the X-Request-Id of the url session

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    Python script that takes in a URL,
+    sends a request to the URL and displays the value of the X-Request-Id
+    variable found in the header of the response
+"""
+
+
+if __name__ == "__main__":
+    from urllib import request
+    import sys
+    req = request.Request(sys.argv[1])
+    with request.urlopen(req) as response:
+        x_required_id = response.getheader("X-Request-Id")
+        print(x_required_id)
