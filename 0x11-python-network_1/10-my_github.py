3b5158090086bd05bd763c0874cd30a8f2cf4ed5
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-04-01 19:06:34 +0000

    feat: use GitHub API

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Python script that takes your GitHub credentials
+    (username and password) and uses the GitHub API
+    to display your id
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    password = sys.argv[2]
+    username = sys.argv[1]
+    response = requests.get("https://api.github.com/user", auth=(username, password))
+    if response.status_code == 200:
+        data = response.json()
+        print(data.get('id'))
+    else:
+        print("None")
