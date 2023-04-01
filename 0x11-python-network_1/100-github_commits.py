b5ec0d3b9cf909d238024fce9af938b5c61070ce
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-04-01 20:20:43 +0000

    fix: api code refactor

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,12 +11,13 @@ import requests
 if __name__ == "__main__":
     repo_name = sys.argv[1]
     owner_name = sys.argv[2]
-    response = requests.get(f"https://api.github.com/repos/\
-                            {owner_name}/{repo_name}/commits")
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+    response = requests.get(url)
     data = response.json()
     try:
         for i in range(10):
-            print("{}: {}".format(data[i]["sha"],
-                  data[i]["commit"]["author"]["name"]))
+            sha = data[i]["sha"]
+            name = data[i]["commit"]["author"]["name"]
+            print(f"{sha}: {name}")
     except IndexError:
         pass
