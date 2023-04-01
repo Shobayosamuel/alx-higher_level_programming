de1fc85daa92c6bda533be631868b8a163f25d60
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-04-01 01:14:42 +0000

    feat: POST an email using requests

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,11 +4,11 @@
     sends a request to the URL and displays
     the body of the response (decoded in utf-8).
 """
+import sys
+import urllib
 
 
 if __name__ == "__main__":
-    import urllib
-    import sys
     try:
         with urllib.request.urlopen(sys.argv[1]) as response:
             content = response.read().decode('utf-8')
