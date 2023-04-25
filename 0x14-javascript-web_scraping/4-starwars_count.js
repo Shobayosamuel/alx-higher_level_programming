e5f4e35a92d0dc580283cb84fed256ea93275b2b
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-04-25 22:27:08 +0000

    fix: output bug

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,22 @@
 #!/usr/bin/node
 
 const request = require('request');
-
 const apiUrl = process.argv[2];
-const characterId = 18;
+const characterId = '18';
+let characterCount = 0;
 
-request(apiUrl, (error, response, body) => {
+request.get(apiUrl, (error, response, body) => {
   if (error) {
-    console.error(error);
+    console.log(error);
   } else {
-    const films = JSON.parse(body).results;
-    const wedgeMovies = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`));
-    console.log(wedgeMovies.length);
+    const data = JSON.parse(body);
+    data.results.forEach((film) => {
+      film.characters.forEach((character) => {
+        if (character.includes(characterId)) {
+          characterCount += 1;
+        }
+      });
+    });
+    console.log(characterCount);
   }
 });
