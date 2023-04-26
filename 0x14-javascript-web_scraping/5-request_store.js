465e2ef8a789a31fe7dde222a48aa74a78316d11
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-04-26 22:58:40 +0000

    fix: corrected semistandard error

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,13 +5,13 @@ const fs = require('fs');
 const url = process.argv[2];
 const file = process.argv[3];
 request(url, function (error, response, body) {
-	if (error) {
-		console.log(error);
-	} else {
-		fs.writeFile(file, body, 'utf-8', function (error) {
-		if (error) {
-		console.log(error);
-		}
-		})
-	}
-})
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(file, body, 'utf-8', function (error) {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
